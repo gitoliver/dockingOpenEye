965ddecf712b58e7ef9a995289869ff549470735
gitoliver <olgrant@uga.edu>
2025-12-11 11:25:47 +0100

    Removed symlink creation part

--- 0.libraryPrep/1.addCharges.bash
+++ 0.libraryPrep/1.addCharges.bash
@@ -1,15 +1,20 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
-   echo "Usage: $0 library file list"
-   echo "Exmpl: $0 shortNameList.txt"
+   echo "Usage: $0 file.csv"
+   echo "Exmpl: $0 shortNameLibraryList.csv"
    exit 1
 fi
-libFile=$1
+csvFile=$1
 
-for lib in $(cat "$libFile");
+>shortNameList.txt
+for line in $(cat "$csvFile");
 do
+    shortName=$(echo $line | cut -f1 -d ,)
+    lib=$(echo $line | cut -f2 -d ,)
+    printf "%s, %s\n" $shortName $lib
+    echo $shortName >> shortNameList.txt
     echo "Adding charges for ${lib} using molcharge."
-    molcharge -in $lib -out ${lib}_charge.oeb.gz
+    molcharge -in $lib -out ${shortName}_charge.oeb.gz
     echo "Charges completed for ${lib}"
 done
 echo "$fin"
