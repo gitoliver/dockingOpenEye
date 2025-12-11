9e9c84bd636e7fb8be41c9cb1dc2f3f4780f9004
gitoliver <olgrant@uga.edu>
2025-12-11 11:43:04 +0100

    Passing in ncpus to omega torsion generation

--- 0.libraryPrep/2.addConformers.bash
+++ 0.libraryPrep/2.addConformers.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-   echo "Usage: $0 library file list"
-   echo "Exmpl: $0 shortNameList.txt"
+if [ $# -ne 2 ]; then
+   echo "Usage: $0 library file list ncpus"
+   echo "Exmpl: $0 shortNameList.txt 10"
    exit 1
 fi
 libFile=$1
-
+ncpus=$2
 for lib in $(cat "$libFile");
 do
-    echo "Adding conformers for ${lib} using omega."
-    oeomega classic -maxconfs 25 -in ${lib}_charge.oeb.gz -out ${lib}_charge_conf.oeb.gz -mpi_np 10
+    echo "Adding conformers for ${lib} using omega. Note mpirun must be available."
+    oeomega classic -maxconfs 25 -in ${lib}_charge.oeb.gz -out ${lib}_charge_conf.oeb.gz -mpi_np $ncpus
     echo "Conformers completed for ${lib}"
 done
 echo "$fin"
